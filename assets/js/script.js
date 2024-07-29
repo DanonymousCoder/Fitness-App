183b4fe1d0329173196662197221058606fa8f4f
Ridwan <aladebaruwa@gmail.com>
2024-07-29 14:36:23 +0100

    Added JavaScript code for navbar toggle functionality and updated HTML structure with new main and hero section elements.

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * event listener on html elements
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    }
+    else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const classNavbar = function () {
+    navbar.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", classNavbar);
